d52883956fa04ed7c152e17823fd225680253fc0
alvin <alvin.tutupary@esb.co.id>
2024-09-08 22:10:43 +0700

    update HomeScreen using component

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,13 @@
+import { MemberListItem } from '@/components/MemberListItem';
 import { MemberModel } from '@/models/MemberModel';
-import { Link } from 'expo-router';
 import { useEffect, useState } from 'react';
-import { View, ScrollView, Text, StyleSheet, ActivityIndicator, Image } from 'react-native';
+import { View, ScrollView, ActivityIndicator } from 'react-native';
 
 
 export default function HomeScreen() {
     const [isLoading, setLoading] = useState(true);
     const [data, setData] = useState<MemberModel[]>([]);
-    
+
     const getMembers = async () => {
         try {
             const response = await fetch('https://dummyjson.com/users');
@@ -19,42 +19,28 @@ export default function HomeScreen() {
             setLoading(false);
         }
     };
-    
+
     useEffect(() => {
         getMembers();
     }, []);
-    
+
 
     return (
         <ScrollView>
-            <View style={styles.header} />
+            <View style={{height: 100}} />
             {
                 isLoading ? <ActivityIndicator /> :
-                data.map((member) => (
-                        <View style={styles.container} key={member.id}>
-                            <Image source={{uri: member.image}} style={styles.memberImage}/>
-                            <Text>Name : {`${member.firstName} ${member.firstName}`}</Text>
-                            <Text>Email : {member.email}</Text>
-                        </View>
+                    data.map((member) => (
+                        <MemberListItem
+                            id={member.id}
+                            image={member.image}
+                            firstName={member.firstName}
+                            lastName={member.lastName}
+                            email={member.email}
+                        />
                     ))
             }
 
         </ScrollView>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        margin: 15,
-    },
-    header: {
-        height: 100
-    },
-    memberImage:{
-        width: 50, 
-        height: 50
-    }
-});
