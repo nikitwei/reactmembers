e3f7be9797f25a2f227603f7789cb4c102fb7020
alvin <alvin.tutupary@esb.co.id>
2024-09-08 23:55:54 +0700

    modify MemberModel

--- models/MemberModel.tsx
+++ models/MemberModel.tsx
@@ -3,14 +3,10 @@ export interface MemberModel {
     firstName: string;
     lastName: string;
     age: number;
-    gender: string;
     email: string;
     phone: string;
     birthDate: string;
     image: string;
-    bloodGroup: string;
-    height: number;
-    weight: number;
     macAddress: string;
     university: string;
 }
