177fded7241fef9bc5bfd3ad923608f92899fc5c
alvin <alvin.tutupary@esb.co.id>
2024-09-08 21:53:13 +0700

    using model interface

--- app/index.tsx
+++ app/index.tsx
@@ -1,3 +1,4 @@
+import { MemberModel } from '@/models/MemberModel';
 import { Link } from 'expo-router';
 import { useEffect, useState } from 'react';
 import { View, ScrollView, Text, StyleSheet, ActivityIndicator, Image } from 'react-native';
@@ -5,7 +6,7 @@ import { View, ScrollView, Text, StyleSheet, ActivityIndicator, Image } from 're
 
 export default function HomeScreen() {
     const [isLoading, setLoading] = useState(true);
-    const [data, setData] = useState<[]>([]);
+    const [data, setData] = useState<MemberModel[]>([]);
     
     const getMembers = async () => {
         try {
@@ -30,10 +31,10 @@ export default function HomeScreen() {
             {
                 isLoading ? <ActivityIndicator /> :
                 data.map((member) => (
-                        <View style={styles.container} key={member["id"]}>
-                            <Image source={{uri: member["image"]}} style={styles.memberImage}/>
-                            <Text>Name : {`${member["firstName"]} ${member["lastName"]}`}</Text>
-                            <Text>NIK : {member["email"]}</Text>
+                        <View style={styles.container} key={member.id}>
+                            <Image source={{uri: member.image}} style={styles.memberImage}/>
+                            <Text>Name : {`${member.firstName} ${member.firstName}`}</Text>
+                            <Text>Email : {member.email}</Text>
                         </View>
                     ))
             }
---
+++ models/MemberModel.tsx
@@ -0,0 +1,16 @@
+export interface MemberModel {
+    id: number;
+    firstName: string;
+    lastName: string;
+    age: number;
+    gender: string;
+    email: string;
+    phone: string;
+    birthDate: string;
+    image: string;
+    bloodGroup: string;
+    height: number;
+    weight: number;
+    macAddress: string;
+    university: string;
+}
