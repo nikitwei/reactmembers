bf8c6453d1700f82f1d6d9ee8831a7a14424aabb
alvin <alvin.tutupary@esb.co.id>
2024-09-08 23:13:09 +0700

    modify member list

--- components/MemberListItem.tsx
+++ components/MemberListItem.tsx
@@ -11,27 +11,75 @@ type MemberListItemModel = {
 
 export function MemberListItem(props: MemberListItemModel) {
   return (
-    <Link href={`/details/${props.id}`} style={styles.link}>
-      <View style={styles.container} key={props.id}>
-        <Image source={{ uri: props.image }} style={styles.memberImage} />
-        <Text>Name : {`${props.firstName} ${props.lastName}`}</Text>
-        <Text>Email : {props.email}</Text>
+    <View style={styles.card}>
+      <Image style={styles.image} source={{ uri: props.image }} />
+      <View style={styles.cardMember}>
+        <Text style={styles.name}>{`${props.firstName} ${props.lastName}`}</Text>
+        <Text style={styles.email}>{props.email}</Text>
+        <Link href={`/details/${props.id}`}
+          style={styles.ButtonDetail}>
+          <Text style={styles.ButtonDetailText}>show details</Text>
+        </Link>
       </View>
-    </Link>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  link: {
-    marginHorizontal:'auto',
-    marginBottom:20
+  cardMember: {
+    marginLeft: 20,
+    marginTop: 10,
   },
-  container: {
-    display:'flex',
-    flexDirection:'row'
+
+  image: {
+    width: 90,
+    height: 90,
+    borderRadius: 45,
   },
-  memberImage: {
-    width: 50,
-    height: 50
-  }
+
+  card: {
+    shadowColor: '#00000021',
+    shadowOffset: {
+      width: 0,
+      height: 6,
+    },
+    shadowOpacity: 0.37,
+    shadowRadius: 7.49,
+    elevation: 12,
+    marginVertical: 10,
+    marginHorizontal: 20,
+    backgroundColor: 'white',
+    flexBasis: '46%',
+    padding: 10,
+    flexDirection: 'row',
+  },
+
+  name: {
+    fontSize: 18,
+    flex: 1,
+    alignSelf: 'flex-start',
+    color: '#008080',
+    fontWeight: 'bold',
+  },
+
+  email: {
+    fontSize: 14,
+    flex: 1,
+    alignSelf: 'flex-start',
+    color: '#696969',
+  },
+
+  ButtonDetail: {
+    marginTop: 10,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    width: 115,
+    borderRadius: 30,
+    backgroundColor: '#00BFFF',
+  },
+
+  ButtonDetailText: {
+    color: '#FFFFFF',
+    fontSize: 16,
+  },
 });
