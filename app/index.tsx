c000af0e0d09208a9ec29a7019bc1e9124d77e0e
alvin <alvin.tutupary@esb.co.id>
2024-09-08 20:49:49 +0700

    test consume dummyData

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,16 @@
 import { Link } from 'expo-router';
 import { View, ScrollView, Text, StyleSheet } from 'react-native';
 
+const dummyData = {
+    name : "John Doe",
+    nik : "20240909"
+}
+
 export default function HomeScreen() {
     return (
         <View style={styles.container}>
-            <Text>Hello Guys</Text>
+            <Text>Name : {dummyData.name}</Text>
+            <Text>NIK : {dummyData.nik}</Text>
         </View>
     );
 }
